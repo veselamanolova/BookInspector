ac8aba7dd386de1b0221ebaca1682ca3381eeff0
veselamanolova <veselamanolov@abv.bg>
2019-04-09 11:27:07 +0300

    Working JsonBooksImporterService

--- Project-BookInspector/BookInspector.Services/JsonBooksImporterService.cs
+++ Project-BookInspector/BookInspector.Services/JsonBooksImporterService.cs
@@ -6,6 +6,7 @@
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Text;
 
     public class JsonBooksImporterService : IJsonBooksImporterService
@@ -37,21 +38,91 @@
                 if (authors == null || authors.Count == 0) // skip importing this book without authors
                     continue;
 
+                List<string> categories= FindCategories(bookToken);
+                if (categories == null || categories.Count == 0) // skip importing this book without authors
+                    continue;
+
+                string publisher = FindPublisher(bookToken);
+                if (string.IsNullOrEmpty(publisher)) // skip importing this book without ISBN
+                    continue;
+
                 string isbn = FindIsbn(bookToken);
                 if (string.IsNullOrEmpty(isbn)) // skip importing this book without ISBN
                     continue;
 
+              
 
-             //   var book = _bookService.AddBook(title, authors);
-             //  result.Add(book);
+              string publishedDateAsString = FindPublishedDateAsString(bookToken);
+                if (string.IsNullOrEmpty(publishedDateAsString)) // skip importing this book without ISBN
+                    continue;
+                DateTime publishedDate = ConvertStringToDate(publishedDateAsString);
+
+                int volumeId = 1;
+
+                string pageCountAsString = FindPageCount(bookToken);
+                if (string.IsNullOrEmpty(publishedDateAsString)) // skip importing this book without ISBN
+                    continue;
+                int.TryParse(pageCountAsString, out int pageCount); 
+
+
+                string description = FindDescription(bookToken);
+                if (string.IsNullOrEmpty(description)) // skip importing this book without ISBN
+                    continue;
+
+
+                var book = _bookService.AddBook(title, authors, categories, publisher, publishedDate, isbn, volumeId, pageCount, description);
+                result.Add(book);
             }
 
             return result;
         }
 
+        private string FindPageCount(JToken bookToken)
+        {
+            return bookToken.SelectToken("volumeInfo.pageCount").ToString();
+        }
+
+        private string FindDescription(JToken bookToken)
+        {
+            return bookToken.SelectToken("volumeInfo.description").ToString();
+        }
+
+        private string FindPublishedDateAsString(JToken bookToken)
+        {
+           return bookToken.SelectToken("volumeInfo.publishedDate").ToString();    
+           
+        }
+
+        private DateTime ConvertStringToDate(string dateAsString)
+        {
+            int[] dateAsArr = dateAsString.ToString().Split('-').Select(int.Parse).ToArray();
+            if (dateAsArr.Count() == 1)
+            {
+                return new DateTime(dateAsArr[0], 1,1);
+            }
+
+            return new DateTime(dateAsArr[0], dateAsArr[1] , dateAsArr[2] );
+
+        }
+
+        private string FindPublisher(JToken bookToken)
+        {
+            return bookToken.SelectToken("volumeInfo.publisher").ToString();
+        }
+
         private static string FindTitle(JToken bookToken)
         {
             return bookToken.SelectToken("volumeInfo.title").ToString();
+        }
+
+        private static List<string> FindCategories(JToken bookToken)
+        {
+            var categories = new List<string>();
+            foreach (JToken categoryToken in bookToken.SelectToken("volumeInfo.categories"))
+            {
+                categories.Add(categoryToken.ToString());
+            }
+            return categories;
         }
 
         private static List<string> FindAuthors(JToken bookToken)
