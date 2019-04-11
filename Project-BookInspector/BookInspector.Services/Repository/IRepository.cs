d981703e41cd2de736ee414ef818d4152ac66962
Anastas Kosstow <anastas.kosstow@protonmail.com>
2019-04-11 11:07:04 +0300

    Repository update

--- Project-BookInspector/BookInspector.Services/Repository/IRepository.cs
+++ Project-BookInspector/BookInspector.Services/Repository/IRepository.cs
@@ -2,20 +2,9 @@
 namespace BookInspector.Services.Repository
 {
     using System.Linq;
-    using System.Threading.Tasks;
 
     public interface IRepository<T> where T : class
     {
         IQueryable<T> All();
-
-        void Add(T entity);
-
-        Task AddAsync(T entity);
-
-        void Update(T entity);
-
-        void Save();
-
-        Task SaveAsync();
     }
 }
--- Project-BookInspector/BookInspector.Services/Repository/Respository.cs
+++ Project-BookInspector/BookInspector.Services/Repository/Respository.cs
@@ -2,10 +2,7 @@
 namespace BookInspector.Services.Repository
 {
     using System.Linq;
-    using System.Threading.Tasks;
     using BookInspector.Data.Context;
-    using Microsoft.EntityFrameworkCore;
-    using Microsoft.EntityFrameworkCore.ChangeTracking;
 
     public class Repository<T> : IRepository<T> where T : class
     {
@@ -19,46 +16,6 @@ namespace BookInspector.Services.Repository
         public IQueryable<T> All()
         {
             return _context.Set<T>();
-        }
-
-        public async Task AddAsync(T entity)
-        {
-            EntityEntry entry = _context.Entry(entity);
-
-            if (entry.State != EntityState.Detached)
-                entry.State = EntityState.Added;
-
-            else await _context.Set<T>().AddAsync(entity);
-        }
-
-        public void Update(T entity)
-        {
-            EntityEntry entry = _context.Entry(entity);
-
-            if (entry.State == EntityState.Detached)
-                _context.Set<T>().Attach(entity);
-
-            entry.State = EntityState.Modified;
-        }
-
-        public async Task SaveAsync()
-        {
-            await _context.SaveChangesAsync();
-        }
-
-        void IRepository<T>.Add(T entity)
-        {
-            EntityEntry entry = _context.Entry(entity);
-
-            if (entry.State != EntityState.Detached)
-                entry.State = EntityState.Added;
-
-            else _context.Set<T>().Add(entity);
-        }
-
-        void IRepository<T>.Save()
-        {
-            _context.SaveChanges();
         }
     }
 }
