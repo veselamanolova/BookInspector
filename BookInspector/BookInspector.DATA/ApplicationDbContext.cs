11587aa9a886dc13f52fe83ae5eebf8d08db5b31
Anastas Kosstow <anastas.kosstow@protonmail.com>
2019-05-02 17:44:59 +0300

    Add db reference

--- BookInspector/BookInspector.DATA/ApplicationDbContext.cs
+++ BookInspector/BookInspector.DATA/ApplicationDbContext.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-using BookInspector.Models;
-
+﻿
 namespace BookInspector.Data
 {
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using BookInspector.Models;
+
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
